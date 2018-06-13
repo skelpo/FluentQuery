32b2e9343ca04519fe7e32474d00fc7aa711e6dc
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-13 14:53:18 -0500

    Created FQWhere(_:[FQPredicateGenericType]) initializer method

--- Sources/FluentQuery/FQWhere.swift
+++ Sources/FluentQuery/FQWhere.swift
@@ -10,13 +10,17 @@ public class FQWhere: FQPart {
         }
     }
     
-    public init(_ predicates: FQPredicateGenericType...) {
+    public init(_ predicates: [FQPredicateGenericType]) {
         for (index, predicate) in predicates.enumerated() {
             if index > 0 {
                 wheres.append("AND")
             }
             wheres.append(predicate)
         }
+    }
+    
+    public convenience init(_ predicates: FQPredicateGenericType...) {
+        self.init(predicates)
     }
     
     @discardableResult
